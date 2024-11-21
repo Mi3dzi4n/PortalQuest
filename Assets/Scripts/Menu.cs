620490d3d4c209a5013c16a2243193722ce247cd
Kubiarek <mateusz.kubiak008@gmail.com>
2024-11-22 00:57:42 +0100

    select level + tilemapa

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -4,11 +4,43 @@ public class MainMenu : MonoBehaviour
 {
     public void Graj()
     {
-        SceneManager.LoadSceneAsync(0);
+        SceneManager.LoadSceneAsync(1);
     }
 
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void LevelsToMenu()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+
+    public void level1()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+    public void level2()
+    {
+        SceneManager.LoadSceneAsync(3);
+    }
+
+    public void level3()
+    {
+        SceneManager.LoadSceneAsync(4);
+    }
+    public void level4()
+    {
+        SceneManager.LoadSceneAsync(5);
+    }
+
+    public void level5()
+    {
+        SceneManager.LoadSceneAsync(6);
+    }
+    public void level6()
+    {
+        SceneManager.LoadSceneAsync(7);
     }
 }
